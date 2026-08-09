8d95528b63876a8d4575d797620595d9943e54c0
eb <34_30@kbs.hs.kr>
2026-08-09 03:30:10 +0000

    자고싶엉

--- __pycache__/모두의알고리즘/모두의 알고리즘1장.py
+++ __pycache__/모두의알고리즘/모두의 알고리즘1장.py
@@ -11,6 +11,7 @@ def abs_Value(num):
 def abs_square(num):
     b = num**2
     return math.sqrt(b)
+# 첫째 마당- 알고리즘 기초
 
 # 1장 1번 - 1부터 n까지의 합구하기
 # 일반적
@@ -28,6 +29,12 @@ def summ(num):
         return 1
     else:
         return num + summ(num-1)
+# 연습문제
+def summul(num):
+    a = 1
+    for i in range(1, num+1):
+        a += i**2
+    return a
 
 # 1장 2번 - 최댓값 찾기
 # 일반적 방법
@@ -44,3 +51,34 @@ def find_max_idx(arr):
         if arr[a] <= arr[i]:
             a = i
     return a
+# 연습문제
+# num = map(int, input().split())
+def inputmax(num):
+    a = num[0]
+    for n in num:
+        if a < n:
+            a = n
+    return n
+
+# 3장 동명이인 찾기
+# 두 번 이상 나온 이름 찾기
+def finddouble(name):
+    n = len(name)
+    a = set()
+    for i in range(n-1):
+        for j in range(i+1, n):
+            if name[i] == name[j]:
+                a.add(a[i])
+    return a
+# 연습문제-짝 지어주기
+def pair(name):
+    n = len(name)
+    # a = set()
+    for i in range(n-1):
+        for j in range(i+1, n):
+            print(name[i], '-', name[j])
+    #         aa = set()
+    #         aa.add(name[i])
+    #         aa.add(name[j])
+    #         a.add(aa)
+    # return a
